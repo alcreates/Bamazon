9f21e190970837d99264fba53864991a9bc833e3
Alvaro Martinez <alvaromartinez@Alvaros-MBP-2.home>
2016-04-17 20:48:01 -0400

    current selection formmated

--- BamazonCustomers.js
+++ BamazonCustomers.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql');
 var prompt = require('prompt');
+var color = require('colors');
 var connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
@@ -24,9 +25,18 @@ function bamazon(){
 
 	connection.query("SELECT * FROM Bamazon.Products WHERE  StockQuantity > 0 ", function(err, results){
 		if(err) throw err;
-
-	
-		console.log(results);
+		console.log("Bamazon.com's current selection---".underline.red);
+		
+		for (var i = 0; i < results.length; i++) {
+			console.log("Id: " + results[i].ItemID);
+			console.log("Product: " + results[i].ProductName);
+			console.log("Deparment: " + results[i].DeparmentName);
+			console.log("Price: " + results[i].Price);
+			console.log("StockQuantity: " + results[i].StockQuantity);
+			console.log("---------------------------------------------".green);
+			console.log("\n");
+		}
+		
 	
 	});
 	
